0749a4fe202addfe3c4163f2c73aafd1ecc2a946
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2016-11-06 01:55:37 +0100

    Follow Emacs naming convention

--- dashboard.el
+++ dashboard.el
@@ -1,4 +1,4 @@
-;;; dashboard.el --- Emacs startup screen extracted from Spacemacs
+;;; dashboard.el --- A startup screen extracted from Spacemacs
 
 ;; Copyright (c) 2016 Rakan Al-Hneiti & Contributors
 ;;
@@ -12,7 +12,7 @@
 ;; Created: October 05, 2016
 ;; Modified: October 06, 2016
 ;; Version: 1.0.0
-;; Keywords: startup screen
+;; Keywords: startup screen tools
 ;; Package-Requires: ((emacs "24.4") (page-break-lines "0.11.0") (projectile "0.14.0"))
 ;;; Commentary:
 ;;; A shameless extraction of Spacemacs’ startup screen.
@@ -45,10 +45,10 @@
   (setq buffer-read-only t
         truncate-lines t))
 
-(defconst banner-length 75
+(defconst dashboard-banner-length 75
 	  "Width of a banner.")
 
-(defun insert-ascii-banner-centered (file)
+(defun dashboard-insert-ascii-banner-centered (file)
   "Insert banner from FILE."
   (insert-string
    (with-temp-buffer
@@ -66,14 +66,14 @@
            (forward-line 1))))
      (buffer-string))))
 
-(defun insert-banner ()
+(defun dashboard-insert-banner ()
   "Insert Banner at the top of the dashboard."
   (insert-ascii-banner-centered
    (concat (file-name-directory
 	    (locate-library "dashboard"))
 	   "banner.txt")))
 
-(defun dashboard/insert-startupify-lists ()
+(defun dashboard-insert-startupify-lists ()
   "Insert the list of widgets into the buffer."
   (interactive)
   (with-current-buffer (get-buffer-create "*dashboard*")
@@ -81,24 +81,24 @@
           (list-separator "\n\n"))
       (goto-char (point-max))
       (insert-banner)
-      (dashboard/insert-page-break)
+      (dashboard-insert-page-break)
 
       (recentf-mode)
-      (when (dashboard//insert-file-list "Recent Files:" (recentf-elements 5))
-	(dashboard//insert--shortcut "r" "Recent Files:")
-	(dashboard/insert-page-break))
+      (when (dashboard-insert-file-list "Recent Files:" (recentf-elements 5))
+	(dashboard-insert--shortcut "r" "Recent Files:")
+	(dashboard-insert-page-break))
 
-      (when (dashboard//insert-bookmark-list "Bookmarks:" (bookmark-all-names))
-	(dashboard//insert--shortcut "m" "Bookmarks:")
-	(dashboard/insert-page-break))
+      (when (dashboard-insert-bookmark-list "Bookmarks:" (bookmark-all-names))
+	(dashboard-insert--shortcut "m" "Bookmarks:")
+	(dashboard-insert-page-break))
 
       (projectile-mode)
-      (when (dashboard//insert-project-list "Projects:" (projectile-relevant-known-projects))
-	(dashboard//insert--shortcut "p" "Projects:")
-	(dashboard/insert-page-break)))
+      (when (dashboard-insert-project-list "Projects:" (projectile-relevant-known-projects))
+	(dashboard-insert--shortcut "p" "Projects:")
+	(dashboard-insert-page-break)))
     (dashboard-mode)))
 
-(defun dashboard//insert-file-list (list-display-name list)
+(defun dashboard-insert-file-list (list-display-name list)
   "Render LIST-DISPLAY-NAME title and items of LIST."
   (when (car list)
     (insert list-display-name)
@@ -114,7 +114,7 @@
                            (abbreviate-file-name el)))
           list)))
 
-(defun dashboard//insert-project-list (list-display-name list)
+(defun dashboard-insert-project-list (list-display-name list)
   "Render LIST-DISPLAY-NAME title and project items of LIST."
   (when (car list)
     (insert list-display-name)
@@ -130,7 +130,7 @@
                            (abbreviate-file-name el)))
           list)))
 
-(defun dashboard//insert-bookmark-list (list-display-name list)
+(defun dashboard-insert-bookmark-list (list-display-name list)
   "Render LIST-DISPLAY-NAME title and bookmarks items of LIST."
   (when (car list)
     (insert list-display-name)
@@ -147,11 +147,11 @@
                                                  (bookmark-get-filename el)))))
           list)))
 
-(defun dashboard/insert-page-break ()
+(defun dashboard-insert-page-break ()
   "Insert a page break line in dashboard buffer."
-  (dashboard/append "\n\f\n"))
+  (dashboard-append "\n\f\n"))
 
-(defun dashboard/append (msg &optional messagebuf)
+(defun dashboard-append (msg &optional messagebuf)
   "Append MSG to dashboard buffer.
 If MESSAGEBUF is not nil then MSG is also written in message buffer."
   (with-current-buffer (get-buffer-create "*dashboard*")
@@ -159,7 +159,7 @@ If MESSAGEBUF is not nil then MSG is also written in message buffer."
     (let ((buffer-read-only nil))
       (insert msg))))
 
-(defmacro dashboard//insert--shortcut (shortcut-char search-label &optional no-next-line)
+(defmacro dashboard-insert--shortcut (shortcut-char search-label &optional no-next-line)
   `(define-key dashboard-mode-map ,shortcut-char (lambda ()
 			       (interactive)
 			       (unless (search-forward ,search-label (point-max) t)
@@ -169,7 +169,7 @@ If MESSAGEBUF is not nil then MSG is also written in message buffer."
 			       (back-to-indentation))))
 
 
-(defun dashboard/goto-link-line ()
+(defun dashboard-goto-link-line ()
   "Move the point to the beginning of the link line."
   (interactive)
   (with-current-buffer "*dashboard*"
@@ -178,13 +178,13 @@ If MESSAGEBUF is not nil then MSG is also written in message buffer."
     (beginning-of-line)
     (widget-forward 1)))
 
-(defun dashboard/setup-startup-hook ()
+(defun dashboard-setup-startup-hook ()
   "Add post init processing."
   (add-hook
    'emacs-startup-hook
    (lambda ()
      ;; Display useful lists of items
-     (dashboard/insert-startupify-lists)
+     (dashboard-insert-startupify-lists)
      (page-break-lines-mode 1)
      (goto-char (point-min)))
    (redisplay))
