7db073f60a2505bf49e9b94cfedd235f1e01e471
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2016-11-21 22:28:06 +0100

    Added contribution docs. Removed Cask and dashboard-pkg.el

--- dashboard.el
+++ dashboard.el
@@ -11,7 +11,7 @@
 ;;
 ;; Created: October 05, 2016
 ;; Modified: October 06, 2016
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; Keywords: startup screen tools
 ;; Package-Requires: ((emacs "24.4") (page-break-lines "0.11"))
 ;;; Commentary:
