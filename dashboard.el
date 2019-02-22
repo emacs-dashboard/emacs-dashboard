e81acaacfa671462fc2c3f5bcc9673805ef0cdb6
Jason Dufair <jase@dufair.org>
2019-02-22 15:57:45 -0500

    Reverse one bad reversal

--- dashboard.el
+++ dashboard.el
@@ -105,11 +105,11 @@
   "Insert the list of widgets into the buffer."
   (interactive)
   (let ((buffer-exists (buffer-live-p (get-buffer dashboard-buffer-name)))
-	(save-line nil)
-	(recentf-is-on (recentf-enabled-p))
-	(origial-recentf-list recentf-list)
-	(dashboard-num-recents (or (cdr (assoc 'recents dashboard-items)) 0))
-	)
+				(save-line nil)
+				(recentf-is-on (recentf-enabled-p))
+				(origial-recentf-list recentf-list)
+				(dashboard-num-recents (or (cdr (assoc 'recents dashboard-items)) 0))
+				(max-line-length 0))
     ;; disable recentf mode,
     ;; so we don't flood the recent files list with org mode files
     ;; do this by making a copy of the part of the list we'll use
