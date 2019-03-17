2cc501d1ea1f8ab5617397cdc61b1ce760758df2
Rudi Schlatte <rudi@constantly.at>
2019-03-17 17:28:19 +0100

    Fix error in `dashboard-next-section`
    - move function body out of `(interactive)` form

--- dashboard.el
+++ dashboard.el
@@ -91,8 +91,8 @@
 
 (defun dashboard-next-section ()
   "Navigate forward to next section."
-  (interactive
-   (let ((current-position (point))
+  (interactive)
+  (let ((current-position (point))
          (next-section-start nil)
          (section-starts (reverse dashboard--section-starts)))
      (dolist (elt section-starts)
@@ -100,7 +100,7 @@
                   (> elt current-position))
          (setq next-section-start elt)))
      (when next-section-start
-       (goto-char next-section-start)))))
+       (goto-char next-section-start))))
 
 (defun dashboard-maximum-section-length ()
   "For the just-inserted section, calculate the length of the longest line."
