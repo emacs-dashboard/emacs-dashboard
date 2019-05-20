5abb76d7d93249b9c7c15881518361e0866d9dbe
ema2159 <ema2159@gmail.com>
2019-05-20 15:40:08 -0600

    Modified .dir-locals.el file to pass linting checks

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,3 @@
 ((emacs-lisp-mode . ((indent-tabs-mode . nil)
-                     (fill-column . 100))))
+                     (fill-column . 100)
+                     (elisp-lint-indent-specs . ((when-let . 1))))))
