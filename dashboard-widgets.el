e7f76cf1f0dfc285deec9229f274eb0c6c107adf
Jesús Martínez <JesusMtnez@users.noreply.github.com>
2019-06-07 07:08:23 +0200

    Support `straight.el` properly in `dashboard-init-info` (#147)
    * Check package.el using featurep
    * Check using bound-and-true-p package-alist
    * Proper support for straight.el

--- dashboard-widgets.el
+++ dashboard-widgets.el
@@ -102,10 +102,13 @@ Example:
 
 (defvar dashboard-init-info
   ;; Check if package.el was loaded and if package loading was enabled
-  (if (bound-and-true-p package-enable-at-startup)
+  (if (bound-and-true-p package-alist)
       (format "%d packages loaded in %s"
               (length package-activated-list) (emacs-init-time))
-    (format "Emacs started in %s" (emacs-init-time)))
+    (if (and (boundp 'straight--profile-cache) (hash-table-p straight--profile-cache))
+        (format "%d packages loaded in %s"
+                (hash-table-size straight--profile-cache) (emacs-init-time))
+      (format "Emacs started in %s" (emacs-init-time))))
   "Init info with packages loaded and init time.")
 
 (defvar dashboard-footer
