2bd3785638ed9acc001801af75cf7ea6f369fb2c
Kisaragi Hiu <mail@kisaragi-hiu.com>
2020-02-15 02:11:56 +0900

    Fix error when calling dashboard-previous-section before any section
    Place cursor on "Welcome to Emacs!" or just anywhere before the first
    section, then run `dashboard-previous-section` --- it will error out
    (at goto-char) complaining that nil is not a number or marker. This is
    caused by the current version not checking if local variables
    previous-section-start or current-section-start are non-nil (unlike
    dashboard-next-section).

--- dashboard.el
+++ dashboard.el
@@ -89,9 +89,10 @@
       (when (and (not current-section-start)
                  (< elt current-position))
         (setq current-section-start elt)))
-    (goto-char (if (eq current-position current-section-start)
-                   previous-section-start
-                 current-section-start))))
+    (goto-char (or (when (eq current-position current-section-start)
+                     previous-section-start)
+                   current-section-start
+                   current-position))))
 
 (defun dashboard-next-section ()
   "Navigate forward to next section."
