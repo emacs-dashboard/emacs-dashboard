7b4cc35c514d32862192e4035f20a20ebc48a1c5
weebojensen <weebojensen@waifu.club>
2020-07-05 12:50:04 +0200

    Add fix for people using straight.el and package.el in dashboard-init-info

--- dashboard-widgets.el
+++ dashboard-widgets.el
@@ -152,13 +152,16 @@ Example:
 
 (defcustom dashboard-init-info
   ;; Check if package.el was loaded and if package loading was enabled
-  (if (bound-and-true-p package-alist)
+  (if (and (and (boundp 'straight--profile-cache) (hash-table-p straight--profile-cache)) (bound-and-true-p package-alist))
       (format "%d packages loaded in %s"
-              (length package-activated-list) (emacs-init-time))
-    (if (and (boundp 'straight--profile-cache) (hash-table-p straight--profile-cache))
+              (+ (length package-activated-list) (hash-table-size straight--profile-cache)) (emacs-init-time))
+    (if (bound-and-true-p package-alist)
         (format "%d packages loaded in %s"
-                (hash-table-size straight--profile-cache) (emacs-init-time))
-      (format "Emacs started in %s" (emacs-init-time))))
+                (length package-activated-list) (emacs-init-time))
+      (if (and (boundp 'straight--profile-cache) (hash-table-p straight--profile-cache))
+          (format "%d packages loaded in %s"
+                  (hash-table-size straight--profile-cache) (emacs-init-time))
+        (format "Emacs started in %s" (emacs-init-time)))))
   "Init info with packages loaded and init time."
   :type 'boolean
   :group 'dashboard)
