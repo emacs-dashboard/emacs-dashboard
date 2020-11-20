643ee31e75639541d0e0204504f142f1076a9e6d
JenChieh <jcs090218@gmail.com>
2020-11-20 10:35:22 +0800

    Clean up init info.

--- dashboard-widgets.el
+++ dashboard-widgets.el
@@ -159,14 +159,13 @@ Example:
 
 (defcustom dashboard-init-info
   ;; Check if package.el was loaded and if package loading was enabled
-  (if (bound-and-true-p package-alist)
-      (format "%d packages loaded in %s"
-              (length package-activated-list) (emacs-init-time))
-    (if (and (boundp 'straight--profile-cache)
-             (hash-table-p straight--profile-cache))
-        (format "%d packages loaded in %s"
-                (hash-table-size straight--profile-cache) (emacs-init-time))
-      (format "Emacs started in %s" (emacs-init-time))))
+  (let ((time (emacs-init-time)))
+    (cond ((bound-and-true-p package-alist)
+           (format "%d packages loaded in %s" (length package-activated-list) time))
+          ((and (boundp 'straight--profile-cache)
+                (hash-table-p straight--profile-cache))
+           (format "%d packages loaded in %s" (hash-table-size straight--profile-cache) time))
+          (t (format "Emacs started in %s" time))))
   "Init info with packages loaded and init time."
   :type 'boolean
   :group 'dashboard)
