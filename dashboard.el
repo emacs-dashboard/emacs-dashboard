d23a4c563f5db16cd3b0e718ce24f9c051894f7e
Chris <7539871+paxperscientiam@users.noreply.github.com>
2020-12-27 23:20:40 -0500

    bugfix: test for terminal type for mode-map

--- dashboard.el
+++ dashboard.el
@@ -41,12 +41,15 @@
     (define-key map (kbd "C-i") 'widget-forward)
     (define-key map [backtab] 'widget-backward)
     (define-key map (kbd "RET") 'dashboard-return)
-    (define-key map [down-mouse-1] 'widget-button-click)
     (define-key map (kbd "g") #'dashboard-refresh-buffer)
     (define-key map (kbd "}") #'dashboard-next-section)
     (define-key map (kbd "{") #'dashboard-previous-section)
     map)
   "Keymap for dashboard mode.")
+
+(if (display-graphic-p)
+    (define-key dashboard-mode-map [down-mouse-1] 'widget-button-click)
+  (define-key dashboard-mode-map [down-mouse-1] nil))
 
 (defcustom dashboard-after-initialize-hook nil
   "Hook that is run after dashboard buffer is initialized."
