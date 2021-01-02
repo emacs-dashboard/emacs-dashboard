5b09f655cb530f9ea3aff9e8f2fd316edae09025
JenChieh <jcs090218@gmail.com>
2021-01-03 00:38:59 +0800

    Revet and use wrapped mouse click.

--- dashboard.el
+++ dashboard.el
@@ -41,6 +41,7 @@
     (define-key map (kbd "C-i") 'widget-forward)
     (define-key map [backtab] 'widget-backward)
     (define-key map (kbd "RET") 'dashboard-return)
+    (define-key map [down-mouse-1] 'dashboard-widget-button-click)
     (define-key map (kbd "g") #'dashboard-refresh-buffer)
     (define-key map (kbd "}") #'dashboard-next-section)
     (define-key map (kbd "{") #'dashboard-previous-section)
