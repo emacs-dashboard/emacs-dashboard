43593bc048b680c78d2d67c4b28d6d1b352e4c16
JenChieh <jcs090218@gmail.com>
2021-01-09 22:14:15 +0800

    Revert.

--- scripts/test-pkg.el
+++ scripts/test-pkg.el
@@ -2,19 +2,19 @@
 ;; (@* "Prepare" )
 ;;
 
-;; (add-to-list 'load-path "./")
-;; (add-to-list 'load-path "./scripts/")
-;; (require 'pkg-prepare)
+(add-to-list 'load-path "./")
+(add-to-list 'load-path "./scripts/")
+(require 'pkg-prepare)
 
-;; (jcs-ensure-package-installed
-;;  (append
-;;   '(page-break-lines org all-the-icons)
-;;   '(elisp-lint package-lint dash))
-;;  t)
+(jcs-ensure-package-installed
+ (append
+  '(page-break-lines org all-the-icons)
+  '(elisp-lint package-lint dash))
+ t)
 
 ;;
 ;; (@* "Test" )
 ;;
 
-;; (require 'dashboard)
-;; (dashboard-insert-startupify-lists)
+(require 'dashboard)
+(dashboard-insert-startupify-lists)
