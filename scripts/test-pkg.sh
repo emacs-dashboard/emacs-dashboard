55dbe34b4bfb4aedc7d2e863a101d8741833d5d9
JenChieh <jcs090218@gmail.com>
2021-01-09 21:12:30 +0800

    Quote path.

--- scripts/test-pkg.sh
+++ scripts/test-pkg.sh
@@ -2,4 +2,4 @@ echo "Executing package..."
 
 ${EMACS:=emacs} -nw --batch --load "./scripts/test-pkg.el"
 
-emacs --batch -q -l /scripts/pkg-prepare.el -l /elisp-lint.el -f elisp-lint-files-batch --no-package-lint dashboard-widgets.el dashboard.el
+emacs --batch -q -l "./scripts/pkg-prepare.el" -l /elisp-lint.el -f elisp-lint-files-batch --no-package-lint dashboard-widgets.el dashboard.el
