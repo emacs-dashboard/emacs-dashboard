a54884ca2ff3a4a5700765741d33ca18c8044889
JenChieh <jcs090218@gmail.com>
2021-01-09 21:18:33 +0800

    Try clone load path.

--- scripts/test-pkg.bat
+++ scripts/test-pkg.bat
@@ -4,4 +4,6 @@ echo "Executing package..."
 
 emacs -nw --batch --load "./scripts/test-pkg.el"
 
-emacs --batch -q -l "./scripts/pkg-prepare.el" -l /elisp-lint.el -f elisp-lint-files-batch --no-package-lint dashboard-widgets.el dashboard.el
+git clone "https://github.com/gonewest818/elisp-lint"
+
+emacs --batch -q -l "./scripts/pkg-prepare.el" -l "./elisp-lint/elisp-lint.el" -f elisp-lint-files-batch --no-package-lint dashboard-widgets.el dashboard.el
--- scripts/test-pkg.sh
+++ scripts/test-pkg.sh
@@ -2,4 +2,6 @@ echo "Executing package..."
 
 ${EMACS:=emacs} -nw --batch --load "./scripts/test-pkg.el"
 
-emacs --batch -q -l "./scripts/pkg-prepare.el" -l /elisp-lint.el -f elisp-lint-files-batch --no-package-lint dashboard-widgets.el dashboard.el
+git clone "https://github.com/gonewest818/elisp-lint"
+
+emacs --batch -q -l "./scripts/pkg-prepare.el" -l "./elisp-lint/elisp-lint.el" -f elisp-lint-files-batch --no-package-lint dashboard-widgets.el dashboard.el
