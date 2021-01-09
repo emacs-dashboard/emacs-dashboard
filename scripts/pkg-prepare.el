c32f300f3305ce483423c3ef1c1be4f2a9cfa368
JenChieh <jcs090218@gmail.com>
2021-01-09 21:52:29 +0800

    Add standard ELPA.

--- scripts/pkg-prepare.el
+++ scripts/pkg-prepare.el
@@ -4,6 +4,7 @@
 
 (require 'package)
 
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (setq package-enable-at-startup nil
       package-check-signature nil)
