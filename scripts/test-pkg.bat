3ab73378b6c63563feaf367ae7a3b6693ee19327
JenChieh <jcs090218@gmail.com>
2021-01-09 20:54:47 +0800

    Add lint test.

--- scripts/test-pkg.bat
+++ scripts/test-pkg.bat
@@ -3,3 +3,5 @@
 echo "Executing package..."
 
 emacs -nw --batch --load "./scripts/test-pkg.el"
+
+emacs --batch -q -l .emacs/init.el -l /elisp-lint.el -f elisp-lint-files-batch --no-package-lint dashboard-widgets.el dashboard.el
--- scripts/test-pkg.sh
+++ scripts/test-pkg.sh
@@ -1,3 +1,5 @@
 echo "Executing package..."
 
 ${EMACS:=emacs} -nw --batch --load "./scripts/test-pkg.el"
+
+emacs --batch -q -l .emacs/init.el -l /elisp-lint.el -f elisp-lint-files-batch --no-package-lint dashboard-widgets.el dashboard.el
