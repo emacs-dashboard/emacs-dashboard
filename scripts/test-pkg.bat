867482013201cd07eb202ddc0c75fbd094b68b3e
JenChieh <jcs090218@gmail.com>
2021-01-09 21:09:57 +0800

    Load package init.

--- scripts/test-pkg.bat
+++ scripts/test-pkg.bat
@@ -4,4 +4,4 @@ echo "Executing package..."
 
 emacs -nw --batch --load "./scripts/test-pkg.el"
 
-emacs --batch -q -l /elisp-lint.el -f elisp-lint-files-batch --no-package-lint dashboard-widgets.el dashboard.el
+emacs --batch -q -l /scripts/pkg-prepare.el -l /elisp-lint.el -f elisp-lint-files-batch --no-package-lint dashboard-widgets.el dashboard.el
