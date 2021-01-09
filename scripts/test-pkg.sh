534372bf3d44cc851439004ee659a702dbf09471
JenChieh <jcs090218@gmail.com>
2021-01-09 22:10:06 +0800

    Try lint inside file.

--- scripts/test-pkg.sh
+++ scripts/test-pkg.sh
@@ -1,1 +1,1 @@
-echo "Executing package..."${EMACS:=emacs} -nw --batch --load "./scripts/test-pkg.el"git clone "https://github.com/gonewest818/elisp-lint"emacs --batch -q -l "./scripts/test-pkg.el" -l "./elisp-lint/elisp-lint.el" -f elisp-lint-files-batch --no-package-lint dashboard-widgets.el dashboard.el
+echo "Executing package..."${EMACS:=emacs} -nw --batch --load "./scripts/test-pkg.el"
