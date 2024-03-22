592f72ee6c0d2252b72216410e8559e87d1bb14a
DevelopmentCool2449 <163072436+DevelopmentCool2449@users.noreply.github.com>
2024-03-21 18:44:16 -0600

    Update dashboard.el

--- dashboard.el
+++ dashboard.el
@@ -507,6 +507,7 @@ See `dashboard-item-generators' for all items available."
 
         (mapc (lambda (entry)
                 (if (and (listp entry)
+                         (not (eq (car entry) 'closure))
                          (not (eq (car entry) 'lambda)))
                     (apply (car entry) (cdr entry))
                   (funcall entry)))
