3852301f9c6f3104d9cc98389612b5ef3452a7de
Ad <144847922+skissue@users.noreply.github.com>
2024-05-29 16:58:37 -0400

    fix: custom type for dashboard-heading-icons (#532)
    The previous type is wrong, as it's for a list of alists, rather than a single
    alist.

--- dashboard-widgets.el
+++ dashboard-widgets.el
@@ -193,7 +193,7 @@ The value can be one of: `all-the-icons', `nerd-icons'."
 Will be of the form `(list-type . icon-name-string)`.
 If nil it is disabled.  Possible values for list-type are:
 `recents' `bookmarks' `projects' `agenda' `registers'"
-  :type  '(repeat (alist :key-type symbol :value-type string))
+  :type  '(alist :key-type symbol :value-type string)
   :group 'dashboard)
 
 (defcustom dashboard-heading-icon-height 1.2
